defbbd83b022c4c4e5ff2166532b30c743109356
IsaHaElla <148005611+IsaHaElla@users.noreply.github.com>
2023-11-16 16:11:48 +0100

    Build+noch nich funktionierender Tod

---
+++ Assets/02_Scripts/PlayerDeath.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            Destroy(gameObject);
+            LevelManager.instance.Respawn();
+    }
+}
