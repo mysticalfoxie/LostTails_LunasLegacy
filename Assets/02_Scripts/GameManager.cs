99957266247aef3cc8cb160f10bc5032fec922f4
Zarbony <44331842+Zarbony@users.noreply.github.com>
2023-11-21 12:54:21 +0100

    Sort [Scenes] | Add [Start], [Level 1] and [End] Scene | Add to [Build] with [Index] | Add [GameManager] Script

---
+++ Assets/02_Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int currentLevelIndex;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        currentLevelIndex += 1;
+        SceneManager.LoadScene(currentLevelIndex);
+    }
+}
