82b2d7a837f2555fd6f5bd63369b3ae671b0575e
Zarbony <44331842+Zarbony@users.noreply.github.com>
2023-11-27 11:50:04 +0100

    Add Escape Button for Pause Menu || Adjust Scripts

--- Assets/02_Scripts/PauseMenu.cs
+++ Assets/02_Scripts/PauseMenu.cs
@@ -6,21 +6,53 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    public void Pause()
+    bool Paused = false;
+
+    private void Awake()
     {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (Paused == false)
+            {
+                Pause();
+                /*Cursor.visible = false;
+                Time.timeScale = 1.0f;
+                pauseMenu.SetActive(false);
+                Paused = false;*/
+
+            } else
+            {
+                Resume();
+                /*Time.timeScale = 0.0f;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+                Paused = true;*/
+            }
+        }
     }
 
+    public void Pause()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 0.0f;
+        pauseMenu.SetActive(true);
+        Paused = true;
+    }
     public void Resume()
     {
+        Cursor.visible = false;
+        Time.timeScale = 1.0f;
         pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
+        Paused = false;
     }
 
     public void Home()
     {
-            if (Time.timeScale == 0) Time.timeScale = 1;
-            SceneManager.LoadScene(0);
+        if (Time.timeScale == 0) Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/02_Scripts/StartMenu.cs
+++ Assets/02_Scripts/StartMenu.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+
+    //To-Do: How To Play?, Options Button, Menu Sound?
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
     public void Credits()
+    {
+
+    }
+
+    public void Options() //To-Do: Sound Slider!
     {
 
     }
