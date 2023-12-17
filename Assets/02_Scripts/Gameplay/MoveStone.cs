8490d8e5c66766c3e75106db10eb3eaaf39f2b9f
Isabell Hartwig <Isabell.Hartwig@games-academy.de>
2023-12-17 23:19:42 +0100

    MoveStone Script

---
+++ Assets/02_Scripts/Gameplay/MoveStone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStone : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody;
+
+    void Start()
+    {
+        Rigidbody2D Rigidbody = GetComponent<Rigidbody2D>();
+        Rigidbody.constraints = RigidbodyConstraints2D.FreezePositionX;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            if (InteractInput())
+            {
+                Rigidbody2D Rigidbody = GetComponent<Rigidbody2D>();
+                Rigidbody.constraints = RigidbodyConstraints2D.None;
+                Rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+
+        }
+
+    }
+    bool InteractInput()
+    {
+        return Input.GetKey(KeyCode.E);
+    }
+}
