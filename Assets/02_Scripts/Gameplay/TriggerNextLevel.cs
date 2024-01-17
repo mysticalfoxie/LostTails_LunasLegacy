dc54b0ae23638f567c367a8d59fce32bd3cd7eb6
MysticalFoxie <michellebecker2003@gmail.com>
2024-01-17 13:41:24 +0100

    Add Fade-In/Out Animations

--- Assets/02_Scripts/Gameplay/TriggerNextLevel.cs
+++ Assets/02_Scripts/Gameplay/TriggerNextLevel.cs
@@ -4,10 +4,7 @@ public class TriggerNextLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("OnTriggerEnter");
         if (!other.gameObject.CompareTag("NextLevel")) return;
-        Debug.Log("CompareTag(NextLevel)");
         GameManager.Instance.LoadNextLevel();
-        Debug.Log("LoadNextLevel");
     }
 }
