d6f759a0f399e72293b80fa436c9de093682f6c5
Isabell Hartwig <Isabell.Hartwig@games-academy.de>
2024-01-22 14:46:27 +0100

    IntroCutScene mit Text und Bild fertig

--- Assets/02_Scripts/CutScenes/GameController.cs
+++ Assets/02_Scripts/CutScenes/GameController.cs
@@ -31,7 +31,7 @@ public class GameController : MonoBehaviour
                     var isLastScene = lastScene == currentScene;
                     if(allScenes.Last()== currentScene)
                     {
-                        SceneManager.LoadScene(1);
+                        SceneManager.LoadScene(2);
                     }
                     currentScene = currentScene.nextScene;
                     bottomBar.PlayScene(currentScene);
