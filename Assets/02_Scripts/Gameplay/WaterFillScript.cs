290c19a4aeb4054f18c02a0ae208b734926aa025
Isabell Hartwig <Isabell.Hartwig@games-academy.de>
2024-01-23 10:04:57 +0100

    Baumstamm schweben jetzt nachoben

--- Assets/02_Scripts/Gameplay/WaterFillScript.cs
+++ Assets/02_Scripts/Gameplay/WaterFillScript.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class WaterFillScript : MonoBehaviour
 {
     [SerializeField] GameObject Water;
+    [SerializeField] GameObject Baumstamm;
+    [SerializeField] GameObject Baumstamm2;
+    [SerializeField] GameObject Baumstamm3;
+    [SerializeField] GameObject TransformPos;
+    [SerializeField] GameObject TransformPos2;
+    [SerializeField] GameObject TransformPos3;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +27,9 @@ public class WaterFillScript : MonoBehaviour
         if (other.gameObject.CompareTag("WaterButton"))
         {
         Water.SetActive(true);
+            Baumstamm.transform.position = TransformPos.transform.position;
+            Baumstamm2.transform.position = TransformPos2.transform.position;
+            Baumstamm3.transform.position = TransformPos3.transform.position;
         }
     }
 }
