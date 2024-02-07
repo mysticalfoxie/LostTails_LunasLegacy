b05127b352d7bb6e94fb9c5452df66001b54917e
IsaHaElla <Isabell.Hartwig@games-academy.de>
2024-02-07 14:29:20 +0100

    CutScene Key geändert

--- Assets/02_Scripts/CutScenes/GameController.cs
+++ Assets/02_Scripts/CutScenes/GameController.cs
@@ -24,7 +24,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             GameManager.LoadNextLevel();
         }
