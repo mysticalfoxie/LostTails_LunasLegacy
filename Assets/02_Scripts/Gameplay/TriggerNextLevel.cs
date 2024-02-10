32294c9b536e2b65b04f7790b5dce4732becc06a
MysticalFoxie <michellebecker2003@gmail.com>
2024-02-10 16:34:05 +0100

    Remove isas buggy "TriggerTonextLevel"; Add existing working NextLevelTrigger, which includes Fading

--- Assets/02_Scripts/Gameplay/TriggerNextLevel.cs
+++ Assets/02_Scripts/Gameplay/TriggerNextLevel.cs
@@ -1,10 +1,24 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerNextLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("NextLevel")) return;
+        DisablePlayerMovement();
         GameManager.LoadNextLevel();
+    }
+
+    private static void DisablePlayerMovement()
+    {
+        var scene = SceneManager.GetActiveScene();
+        var rootObjects = scene.GetRootGameObjects();
+        var player = rootObjects.FirstOrDefault(x => x.CompareTag("Player"));
+        if (player == null) return;
+        var movement = player.GetComponent<PlayerMovement>();
+        if (movement == null) return;
+        movement.enabled = false;
     }
 }
